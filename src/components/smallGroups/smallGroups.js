bcf01dfa8fb20048fbec36966f3056b7080d3c75
David Berget <davidberget@gmail.com>
2018-05-03 22:39:16 -0500

    hooked up to tnet backend

--- src/components/smallGroups/smallGroups.js
+++ src/components/smallGroups/smallGroups.js
@@ -5,19 +5,33 @@ export default class SmallGroup extends Component {
   state = { type: "" }
 
   handleGroupChange = e => {
+    console.log(e.target.value)
     this.setState({ type: e.target.value })
   }
+
+  handleSubmit = value => {
+    fetch("http://localhost:4000/api", {
+      body: JSON.stringify(value),
+      method: "POST"
+    })
+  }
+
   render() {
     return (
       <div class="formContainer">
         <div class="formBody">
           <h2>New Small Group</h2>
-          <div className={style.formHeader}> Group Type: </div>
-          <select onChange={this.handleGroupChange}>
-            <option>Come and See</option>
-            <option>Follow Me</option>
-            <option>Ongoing Accountabilty Group</option>
-          </select>
+          <div class="formGroup">
+            <div class="formHeader"> Group Type: </div>
+            <select onChange={this.handleGroupChange}>
+              <option value="Come and See">Come and See</option>
+              <option value="Follow Me">Follow Me</option>
+              <option value="Ongoing Accountabilty Group">
+                Ongoing Accountabilty Group
+              </option>
+            </select>
+          </div>
+          <button onClick={() => this.handleSubmit(this.state)}>Submit</button>
         </div>
       </div>
     )
